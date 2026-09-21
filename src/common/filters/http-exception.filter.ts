2d18cca8ae75596f87038b8c854597667d6a2ca2
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2026-09-21 23:25:38 +0400

    Global Exception Filter added error response.

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,45 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import type { Request, Response } from "express";
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost) {
+        const context = host.switchToHttp();
+        const request = context.getRequest<Request>();
+        const response = context.getResponse<Response>();
+        let statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+        let message = 'Internal server error';
+        let error = 'Internal Server Error';
+
+        if (exception instanceof HttpException) {
+            statusCode = exception.getStatus();
+
+            const exceptionResponse = exception.getResponse();
+
+            if (typeof exceptionResponse === 'string') {
+                message = exceptionResponse;
+            } else if (typeof exceptionResponse === 'object' && exceptionResponse !== null) {
+                const responseBody = exceptionResponse as {
+                    message?: string | string[];
+                    error?: string;
+                };
+
+                if (Array.isArray(responseBody.message)) {
+                    message = responseBody.message.join(', ');
+                } else if (responseBody.message) {
+                    message = responseBody.message;
+                }
+
+                if (responseBody.error) error = responseBody.error;
+            }
+        }
+
+        response.status(statusCode).json({
+            statusCode,
+            message,
+            error,
+            timestamp: new Date().toISOString(),
+            path: request.url,
+        })
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -16,6 +17,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+  )
 
   await app.listen(process.env.PORT ?? 3000);
 }
